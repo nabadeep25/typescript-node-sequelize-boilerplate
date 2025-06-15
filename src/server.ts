102c745fc491be1ef0b2f92e81421cdd44364a31
NBDP <47459676+nabadeep25@users.noreply.github.com>
2025-06-15 17:45:22 +0530

    Feat/docker
    * add docker and docker-compose file
    
    * docs: :memo: add docker related information

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "development") {
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"),"0.0.0.0",() => {
   console.log(
     "  App is running at http://localhost:%d in %s mode",
     app.get("port"),
